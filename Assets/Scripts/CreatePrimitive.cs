d263b41e57c09ad77da729417e6195e9f3946131
AmanKukreja <55269952+AmanKukreja@users.noreply.github.com>
2023-07-12 13:27:08 +0100

    Scaling
    Not complete

--- Assets/Scripts/CreatePrimitive.cs
+++ Assets/Scripts/CreatePrimitive.cs
@@ -7,7 +7,16 @@ public class CreatePrimitive : MonoBehaviour
     public GameObject Prefab1;
     public GameObject Prefab2;
     private GameObject Prefab;
-    private float typep = 1f;
+    
+    private float typep = 0f;
+
+    private float DirectionOfScale = 0f;
+    private Vector3 scale;
+
+    public void Start()
+    {
+        scale = new Vector3(0.1f, 0.1f, 0.1f);
+    }
 
     public void SetPrimitiveType(float value)
     {
@@ -29,7 +38,42 @@ public class CreatePrimitive : MonoBehaviour
 
         GameObject Newcube = Instantiate(Prefab);// CreatePrimitive(PrimitiveType.Cube);
         Newcube.transform.position = new Vector3(-0.95f, 0.89f, 1.545f);  //transform.position-new Vector3(0.2f,0,0);
-        Newcube.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+        Newcube.transform.localScale = scale; //new Vector3(0.1f,0.1f,0.1f);
         //Newcube.AddComponent<UnityEngine.XR.Interaction.Toolkit.XRGrabInteractable>();
     }
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    scale = new Vector3(0.1f,0.1f,0.1f);
+    //}
+
+    // Update is called once per frame
+    public void setScale(float value)
+    {
+        if (DirectionOfScale == 0)
+        {
+            scale = new Vector3(0.1f + value, 0.1f, 0.1f);
+            //Debug.Log(GameObject.transform.localScale.x);
+        }
+        else if (DirectionOfScale == 1)
+        {
+            scale = new Vector3(0.1f, 0.1f + value, 0.1f);
+        }
+        else if (DirectionOfScale == 2)
+        {
+            scale = new Vector3(0.1f, 0.1f, 0.1f + value);
+        }
+        else
+        {
+            scale = new Vector3(0.1f + value, 0.1f + value, 0.1f + value);
+        }
+    }
+
+    public void SetDirection(float value)
+    {
+        DirectionOfScale = value;
+        Debug.Log("Direction of Scale"+ DirectionOfScale);
+    }
+
 }
