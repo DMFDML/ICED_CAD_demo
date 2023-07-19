094b92d470f0d2c9788cf01d6240cb8dbff18e44
AmanKukreja <55269952+AmanKukreja@users.noreply.github.com>
2023-07-19 17:24:20 +0100

    Regenerate Carving Blanks

--- Assets/Scripts/RemoveObjectsWithTag.cs
+++ Assets/Scripts/RemoveObjectsWithTag.cs
@@ -8,7 +8,7 @@ public class RemoveObjectsWithTag : MonoBehaviour
     public void RemoveObjects(string tag)
     {
         GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag(tag);
-
+        Debug.Log(taggedObjects);
         foreach (GameObject targetObject in taggedObjects)
             Destroy(targetObject);  
     }
