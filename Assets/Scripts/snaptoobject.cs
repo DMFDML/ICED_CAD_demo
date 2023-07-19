a57289b6a336e0b2aef3f42ab25ad4d7f9d03e9b
AmanKukreja <55269952+AmanKukreja@users.noreply.github.com>
2023-07-19 15:13:11 +0100

    Latest AK
    Working scene Demo Scene 4

--- Assets/Scripts/snaptoobject.cs
+++ Assets/Scripts/snaptoobject.cs
@@ -1,17 +1,43 @@
-// It checks for Istrigger, if it 
-using System.Collections;
-using System.Collections.Generic;
+//// It checks for Istrigger, if it 
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class snaptoobject : MonoBehaviour
+//{
+//    // The snap point on this object
+//    public GameObject combined;
+
+//    private void OnTriggerEnter(Collider namedcol)
+//    {
+//        Debug.Log("trigger entered " + namedcol.gameObject.transform);
+//        namedcol.gameObject.transform.SetParent(combined.transform, true);
+//    }
+//}
 using UnityEngine;
 
 public class snaptoobject : MonoBehaviour
 {
-    // The snap point on this object
-    public GameObject combined;
-
-    private void OnTriggerEnter(Collider namedcol)
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("trigger entered " +namedcol.name);
-        namedcol.transform.SetParent(combined.transform, true);
+        // Check if the other GameObject has a Rigidbody (to ensure it's a movable object).
+        Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
+        if (otherRigidbody != null)
+        {
+            // Make the parent GameObject the new parent of the entered object.
+            other.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the other GameObject has a Rigidbody (to ensure it's a movable object).
+        Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
+        if (otherRigidbody != null)
+        {
+            // Remove the parent-child relationship between the parent and the exited object.
+            other.transform.SetParent(null);
+        }
     }
 }
 
